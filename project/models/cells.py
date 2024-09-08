2c13c02466c1f3885378f304f49eedd9deb1543c
baudrysouvignet <baudrysouvignet07@gmail.com>
2024-09-08 10:50:32 +0200

    feat: Ajout des messages d'erreurs

--- project/models/cells.py
+++ project/models/cells.py
@@ -1,10 +1,15 @@
 class Cells:
-    def __init__(self, x:int, y:int):
+    COORDINATE_TYPE_ERROR_MSG = "The cell coordinates must be a integers"
+    STATE_TYPE_ERROR_MSG = "The state must be a boolean"
+
+    def __init__(self, x:int, y:int,):
+        if not all(isinstance(x, int) for x in (x, y)):
+            raise ValueError(Cells.COORDINATE_TYPE_ERROR_MSG)
         self.r = x
         self.i = y
         self.state = False
 
     def set_state(self, state: bool) -> None:
         if not isinstance(state, bool):
-            raise ValueError("The state must be a boolean")
+            raise ValueError(Cells.STATE_TYPE_ERROR_MSG)
         self.state = state
--- test/test_cells.py
+++ test/test_cells.py
@@ -5,6 +5,12 @@ class TestCells(unittest.TestCase):
     def setUp(self):
         self.cell = Cells(1, 2)
 
+    def test_cell_declaration_invalid(self):
+        with self.assertRaises(ValueError) as context:
+            Cells('1', '2')
+
+        self.assertEqual(str(context.exception), Cells.COORDINATE_TYPE_ERROR_MSG)
+
     def test_set_state_true(self):
         self.cell.set_state(True)
         self.assertTrue(self.cell.state)
@@ -16,8 +22,10 @@ class TestCells(unittest.TestCase):
 
 
     def test_set_state_invalid(self):
-        with self.assertRaises(ValueError):
+        with self.assertRaises(ValueError) as context:
             self.cell.set_state("invalid")
+
+        self.assertEqual(str(context.exception), Cells.STATE_TYPE_ERROR_MSG)
 
 if __name__ == '__main__':
     unittest.main()
